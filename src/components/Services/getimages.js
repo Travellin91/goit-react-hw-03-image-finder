72a75017ae71910985cf8eb102167997777c537c
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-06-11 15:17:43 +0300

    update

--- src/components/Services/getimages.js
+++ src/components/Services/getimages.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '35806799-4c2a0f1dc5f5db1917aa24325';
 
-export async function getImages(searchQuery, page) {
+export function getImages(searchQuery, page) {
   const params = new URLSearchParams({
     key: API_KEY,
     q: searchQuery,
@@ -14,11 +14,11 @@ export async function getImages(searchQuery, page) {
     per_page: 12,
   });
 
-  try {
-    const response = await axios.get(`${BASE_URL}?${params.toString()}`);
-    return response.data;
-  } catch (error) {
-    console.error(error.toJSON());
-    throw error;
-  }
+  return axios
+    .get(`${BASE_URL}?${params.toString()}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(error.toJSON());
+      throw error;
+    });
 }
